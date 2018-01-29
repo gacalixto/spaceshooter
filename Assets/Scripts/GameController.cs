125694f4b889a67ff5dceee4a6543c7b85146f64
Gabriel <gabrielcalixto9@gmail.com>
2018-01-29 13:32:30 -0200

    add hazards and logic for spawning and destroying hazards

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject hazards;
+    public int hazardCount;
+    public Vector3 spawnValues;
+    public float spawnWait,startWait,waveWait;
+    void Start()
+    {
+        StartCoroutine(SpawnWaves ());
+        
+    }
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazards, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+       
+        
+
+    }
+}
